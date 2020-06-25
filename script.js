a94b1433b00fcd75c9fb85d63616f6cd4317b0c6
izdoughty <izdoughty@gmail.com>
2020-06-24 19:02:18 -0500

    Script beautified with simplified notes

--- script.js
+++ script.js
@@ -105,7 +105,7 @@ const eZodiac = {
             earthlyBranch: "h&agrave;i",
             yinYang: "Yin"
         },
-		{
+        {
             name: "Rat",
             earthlyBranch: "zi&solbar;k",
             yinYang: "Yang"
@@ -148,87 +148,62 @@ const eZodiac = {
     ],
     eZElements: ["Metal", "Water", "Wood", "Fire", "Earth"]
 };
-//birthdayValue function takes the input from the form and turns it into a value. 
-//the value then becomes a date.
+//takes the input from the form and turns it into a value, then to a Date 
 function birthdayValue() {
-	//bValue receives the element/data by  form's ID and turns into a value to use
-    let bValue = document.getElementById("birthday").value; 
-	//bValue uses date() to introduce a new date as bDate 
+    //bValue receives the element/data by form's ID and turns into a value to use
+    let bValue = document.getElementById("birthday").value;
+    //bValue uses date() to introduce a new date as bDate 
     let bDate = new Date(bValue);
-	//bDate's Day is off because computers count 0 as a number
-	//Introduce a new date and set the date to be bDate
-	//use getDate() on bDate to get the day and add 1*/
+    //bDate's Day is off because computers start counting at 0
+    //correct it bDate by introducing a new date and using getDate() + 1
     let bAddDay = new Date(bDate.setDate(bDate.getDate() + 1));
-	//Bring each function in so thatbAddDay can be passed onto them
-	//without adding in bAddDay here, the variable is out of scope
-	//once this is done, it can be used
+    //bring each function in so thatbAddDay can be passed onto them
     wZodDate(bAddDay);
     eZodDate(bAddDay);
-	eZodElement(bAddDay);
+    eZodElement(bAddDay);
 }
 
 //inRange is not a pre-made function in JavaScript
-//set a function with 3 parameters - one that is being tested, the min, and max
-//set the tested to be compared to the other variables
+//set 3 parameters - one that is being tested, the min, and max
 function inRange(test, min, max) {
     return (test >= min && test <= max);
 }
 
-//ZodDate() checks the date from birthdayValue()'s bAddDay 
-//(which has been made the parameter to wZodDate() in birthdayValue())
-//bDate is the variable created for this function's scope*/
+//wZodDate() checks the date from birthdayValue()'s bAddDay 
 function wZodDate(bDate) {
-	//orginalYear is created to save the value of bDate (aka bAddDay)
-	let orginalYear = bDate.getFullYear();
-	/*bDate's year is set to 2020 to make 
-	comparing the month and day possible regardless of bDate's year*/
-    bDate.setFullYear(2020); 
-	//bWrite is preset to false for future proofing purposes
+    //orginalYear is created to save the value of bDate
+    let orginalYear = bDate.getFullYear();
+    //bDate's year is set to 2020 to allow date comparison
+    bDate.setFullYear(2020);
+    //bWrite is preset to false for future proofing purposes
     let bWrite = false;
-	//a for loop is created to iterate through wZodiac start and end dates
-	//the length of wZodiac is used as a parameter to stop i from going too far
-    for(i = 0; i < wZodiac.length; i++) {
-		//the create inRnage function is used to compare the dates' range
-		//against the test parameter bDate
-		//new Date()'s used to create dates to compare with bDate's date type
-		//the iterator "i" from the for loop is set as the array object's place
-        if(inRange(bDate, new Date(wZodiac[i].startDate).getTime(), new Date(wZodiac[i].endDate).getTime())) {
-            //write the name of the wZodiac given what has been set from i and bDate
-			//document.write(wZodiac[i].name);
+    //a for loop is created to iterate through wZodiac start and end dates
+    for (i = 0; i < wZodiac.length; i++) {
+        //inRange() is used to compare the dates' range and new Date()'s create dates to compare with bDate's date type
+        if (inRange(bDate, new Date(wZodiac[i].startDate).getTime(), new Date(wZodiac[i].endDate).getTime())) {
             //bWrite is set to true when this is written
-			bWrite = true;
-			//code ends
+            bWrite = true;
+            //code ends
             break;
         }
     }
-	//if bWrite is not true, write the following error message
-    if(!bWrite) {
+    //if bWrite is not true, write error message
+    if (!bWrite) {
         document.write("Entry not found! Please try again.");
     }
-	//set bDate's year to original year.
-	//This will make the rest of the code use the proper year again
-	bDate.setFullYear(orginalYear);
+    //set bDate's year to original year to use the proper year after this function
+    bDate.setFullYear(orginalYear);
 }
 
-//eZodDate() also uses bDate as a function.
-//bDate is used again got reading purposes and this bDate is only for this function's scope
-//whatever the parameter, the information is still from bAddDay as set from within birthdayValue()
+//eZodDate() set the eZodiac animal based on the year
 function eZodDate(bDate) {
-		//the place in the arrary is from getting the year from bDate
-		//then using the remainder from the year given and the length of the eZodiac.animals
-		//no function required to iterate through the information as no comparison is necessary
-        let i = bDate.getFullYear() % eZodiac.animals.length;
-		//write the information using i in the array's place
-        //document.write(eZodiac.animals[i].name, eZodiac.animals[i].earthlyBranch, eZodiac.animals[i].yinYang);
+    //using the remainder from the year given and the length of the eZodiac.animals
+    let i = bDate.getFullYear() % eZodiac.animals.length;
+    //document.write(eZodiac.animals[i].name, eZodiac.animals[i].earthlyBranch, eZodiac.animals[i].yinYang);
 }
 
 //eZodElement() is the same as eZodDate with the exception that the information needs to be repeated twice
-//bDate is used again for the parameter for simple understanding
-function eZodElement(bDate){
-		//Math.floor() rounds bDate's year after it is divided by 2
-		//This is done not only to clean up the number after division but
-		//"slow" the progression through the array as many times as needed (2x)
-		//otherwise it would be the same as eZodDate()
-		let i = Math.floor(bDate.getFullYear() / 2) % eZodiac.eZElements.length;
-        //document.write(eZodiac.eZElements[i]);
+function eZodElement(bDate) {
+    let i = Math.floor(bDate.getFullYear() / 2) % eZodiac.eZElements.length;
+    //document.write(eZodiac.eZElements[i]);
 }
